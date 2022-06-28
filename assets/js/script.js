1e212077bcb8179fb15b7ab2994c3ed604292406
RPAPE3 <106628145+RPAPE3@users.noreply.github.com>
2022-06-28 13:02:51 -0700

    created getUserPreference that will identify which category and difficulty the user selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 
+
+
+var getUserPreference = function () {
+    var category = userInput; 
+    var difficulty = userInput_2; 
+
+    getQuestionData(category, difficulty)
+}
   
   
   
   
-  
-  var getQuestions = function (category, difficulty) {
+  var getQuestionData = function (category, difficulty) {
     var userTriviaData = "https://the-trivia-api.com/api/questions?categories="+ category +"&limit=10&difficulty="+ difficulty;
 
     fetch(userTriviaData)
