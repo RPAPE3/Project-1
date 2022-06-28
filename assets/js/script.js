f7a515ad1ca4c598cbd735be79c2c1b1aa8d9b5a
RPAPE3 <106628145+RPAPE3@users.noreply.github.com>
2022-06-28 12:48:30 -0700

    Added the getQuestions function that fetches the trivia API

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+  
+  
+  
+  
+  
+  var getQuestions = function (category, difficulty) {
+    var userTriviaData = "https://the-trivia-api.com/api/questions?categories="+ category +"&limit=10&difficulty="+ difficulty;
+
+    fetch(userTriviaData)
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+              console.log(data)
+            displayWeather(data);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect the trivia questions');
+      });
+  };
+
+
+
