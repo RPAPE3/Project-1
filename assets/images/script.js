08ea6fba6dc340a17ce333452c74e96b7c19cedb
Stephen Heinen <stephenheinen22@yahoo.com>
2022-06-29 16:20:01 -0500

    took away map momentarily, completed Geocode function with user input

--- assets/images/script.js
+++ assets/images/script.js
@@ -1,11 +1,37 @@
-function initMap() {
-    var location = {
-        lat: 29.512,
-        lng: -98.241
-    }
-    var options = {
-        center: location,
-        zoom: 13
-    }
-    map = new google.maps.Map(document.getElementById("map"), options);
+var locationEl = document.querySelector('#zipCode');
+
+function autoC() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('zipCode'),
+        {
+            types: ['(regions)'],
+            componentRestrictions: { 'country': ['US'] }
+        }
+    );
+    autocomplete.addListener('zipCode');
+}
+
+function geocode () {
+    var location = locationEl.value;
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params:{
+            address: location,
+            key: 'AIzaSyCfxIjECtlR69B-XRURzPSvP9Sdb_Ae73k'
+        }
+    })
+    .then(function(response){
+        console.log(response);
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+}
+
+// var options = {
+//     center: longlat().value,
+//     zoom: 10
+// }
+
+function btn_handler() {
+    geocode();
 }
