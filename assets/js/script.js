69415fbe8b8291e097ac08b0ae2635f07db86717
jsumbak <julsumba311@gmail.com>
2022-07-01 14:14:44 -0700

    commiting recent changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var startBtn = document.querySelector(".start-btn");
+var startBtn = document.querySelector(".glow-on-hover");
 var categoryEl = document.querySelector(".category");
 var difficultyEl = document.querySelector(".difficulty");
 
@@ -10,16 +10,40 @@ var getUserPreference = function () {
 
     var category = categoryEl.value
     var difficulty = difficultyEl.value
-
-    if (category && difficulty) {
-        location.assign(`./question.html?category=${category}&difficulty=${difficulty}`);
+    console.log(category)
+    console.log(difficulty)
+    
+     if  (category === "Categories" || difficulty === "Difficulty Level") {
+        myFunction()
     } else {
-        //NEED TO CHANGE ALERT TO EITHER A HELPER TEST/TEXT? OR SNAKE BAR. 
-        alert("Category and Difficulty but have selections")
+        location.assign(`./question.html?category=${category}&difficulty=${difficulty}`);
     }
+}
   
-};
+function myFunction(){
+    var snackbar = document.getElementById("snackbar");
+    snackbar.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
 
 
 
 startBtn.addEventListener("click", getUserPreference);
+
+// var getUserPreference = function () {
+
+//     var category = categoryEl.value
+//     var difficulty = difficultyEl.value
+// console.log(category)
+// console.log(difficulty)
+//     if (category && difficulty) {
+//         location.assign(`./question.html?category=${category}&difficulty=${difficulty}`);
+//     } else {
+//        myFunction()
+//     }
+  
+// function myFunction(){
+//     var snackbar = document.getElementById("snackbar");
+//     snackbar.className = "show";
+//     setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+//}
